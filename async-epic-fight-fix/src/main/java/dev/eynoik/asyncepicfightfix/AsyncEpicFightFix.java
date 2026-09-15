ba037c8d606c0a8216a9c24db282a9ee00190cbd
meynoik <neynoi.k@gmail.com>
2026-09-15 16:57:31 +0200

    Add Async Epic Fight Fix mod entrypoint

---
+++ async-epic-fight-fix/src/main/java/dev/eynoik/asyncepicfightfix/AsyncEpicFightFix.java
@@ -0,0 +1,12 @@
+package dev.eynoik.asyncepicfightfix;
+
+import net.neoforged.fml.common.Mod;
+
+@Mod(AsyncEpicFightFix.MOD_ID)
+public final class AsyncEpicFightFix {
+    public static final String MOD_ID = "asyncepicfightfix";
+
+    public AsyncEpicFightFix() {
+        System.out.println("[AsyncEpicFightFix] Epic Fight + addon Async compatibility guard enabled.");
+    }
+}
