c3a38473565d2c3cd7ff7ece910289c463910ebf
meynoik <neynoi.k@gmail.com>
2026-09-19 14:12:53 +0200

    Add SkinnedMesh compile-only stub

---
+++ epicfight-twilight-apotheosis-fix/src/stubs/java/yesman/epicfight/api/client/model/SkinnedMesh.java
@@ -0,0 +1,10 @@
+package yesman.epicfight.api.client.model;
+
+/**
+ * Compile-only Epic Fight shape. Not packaged in the runtime JAR.
+ */
+public class SkinnedMesh {
+    public Mesh.RenderProperties getRenderProperties() {
+        return null;
+    }
+}
