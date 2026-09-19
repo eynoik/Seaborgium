164333c206fbb18d672b5793f77dcef72cbcf730
meynoik <neynoi.k@gmail.com>
2026-09-19 14:15:04 +0200

    Keep Epic Fight stub independent of Minecraft classpath

--- epicfight-twilight-apotheosis-fix/src/stubs/java/yesman/epicfight/world/capabilities/entitypatch/LivingEntityPatch.java
+++ epicfight-twilight-apotheosis-fix/src/stubs/java/yesman/epicfight/world/capabilities/entitypatch/LivingEntityPatch.java
@@ -1,12 +1,13 @@
 package yesman.epicfight.world.capabilities.entitypatch;
 
-import net.minecraft.world.entity.LivingEntity;
-
 /**
  * Compile-only shape used by external mixins. This class is not included in the produced mod JAR.
  * The real class is provided by Epic Fight at runtime.
+ *
+ * T intentionally has no Minecraft bound here because the isolated stub source set has no Minecraft
+ * compile classpath. Generic erasure still matches Epic Fight's runtime getOriginal() descriptor.
  */
-public abstract class LivingEntityPatch<T extends LivingEntity> {
+public abstract class LivingEntityPatch<T> {
     public abstract void setYRot(float yRot);
     public abstract T getOriginal();
 }
