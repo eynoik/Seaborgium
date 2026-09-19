f478c5d0f3e9bc3c0c2d333b58498bd32a825d87
meynoik <neynoi.k@gmail.com>
2026-09-19 14:12:45 +0200

    Extend Epic Fight compile-only stub for render gating

--- epicfight-twilight-apotheosis-fix/src/stubs/java/yesman/epicfight/world/capabilities/entitypatch/LivingEntityPatch.java
+++ epicfight-twilight-apotheosis-fix/src/stubs/java/yesman/epicfight/world/capabilities/entitypatch/LivingEntityPatch.java
@@ -1,9 +1,12 @@
 package yesman.epicfight.world.capabilities.entitypatch;
 
+import net.minecraft.world.entity.LivingEntity;
+
 /**
- * Compile-only shape used by the external mixin. This class is not included in the produced mod JAR.
+ * Compile-only shape used by external mixins. This class is not included in the produced mod JAR.
  * The real class is provided by Epic Fight at runtime.
  */
-public abstract class LivingEntityPatch<T> {
+public abstract class LivingEntityPatch<T extends LivingEntity> {
     public abstract void setYRot(float yRot);
+    public abstract T getOriginal();
 }
